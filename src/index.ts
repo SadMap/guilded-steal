0749f1a55e3fb116eb9a57ecf029b2ba7851e2c3
SadMap <71943073+SadMap@users.noreply.github.com>
2022-12-11 16:07:17 +0000

    oops

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,8 @@
 import {Client} from "@guildedts/framework";
+import {readFileSync} from "fs"
 export const client = new Client();
 export let commands = client.commands
 client.on("ready", () => {
     import("./server.js")
 })
-client.login("gapi_LgdqTJG8Of7AurXqLw50JRBHiQbGc4FbS3V7/XWkZfSZkep2CGstJIhggY6++EmXSS1H3Wq/VwzHqEEx4prO4g==")
+client.login(JSON.parse(readFileSync("./gtsconfig.json").toString()).token)
